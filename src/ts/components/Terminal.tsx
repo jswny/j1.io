3d534d78219f013997646eb0f388c023ed7579df
Joe Sweeney <joe@j1.io>
2019-06-29 18:16:46 -0400

    Begin optimizing inputs

--- src/ts/components/Terminal.tsx
+++ src/ts/components/Terminal.tsx
@@ -7,10 +7,7 @@ import "../../css/terminal.css";
 
 export interface TerminalProps { prompt: string; }
 interface Line { input: string; output: string; directory: string; }
-interface TerminalState { 
-  currentText: string; 
-  lines: Array<Line> 
-}
+interface TerminalState { currentText: string; lines: Array<Line> }
 
 export class Terminal extends React.Component<TerminalProps, TerminalState> {
   shell: Shell;
@@ -24,13 +21,13 @@ export class Terminal extends React.Component<TerminalProps, TerminalState> {
     };
   }
 
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleOnKeyDown);
-  }
+  // componentDidMount() {
+  //   document.addEventListener("keydown", this.handleOnKeyDown);
+  // }
 
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleOnKeyDown);
-  }
+  // componentWillUnmount() {
+  //   document.removeEventListener("keydown", this.handleOnKeyDown);
+  // }
 
   private getCurrentDirectoryCopy() {
     const shellCopy = {...this.shell};
@@ -56,12 +53,12 @@ export class Terminal extends React.Component<TerminalProps, TerminalState> {
     this.setState({ lines: lines });
   }
 
-  private handleEnter() {
+  private handleSubmitInput(input: string) {
     let lines = this.state.lines;
     let currentLine = this.getCurrentLine();
-    console.debug(`Terminal sending input "${currentLine.input}" for processing`);
+    console.debug(`Terminal sending input "${input}" for processing`);
     try {
-      currentLine.output = this.shell.command(currentLine.input);
+      currentLine.output = this.shell.command(input);
     } catch (e) {
       if (e instanceof ProgramNotFoundError) {
         currentLine.output = e.message;
@@ -74,29 +71,31 @@ export class Terminal extends React.Component<TerminalProps, TerminalState> {
     this.setState({ lines: lines });
   }
   
-  handleOnKeyDown = (e: KeyboardEvent) => {
-    if (e.keyCode === 13) { // Enter
-      this.handleEnter();
-    } else if (e.keyCode === 8) { // Backspace
-      this.updateCurrentLine((line) => {
-        line.input = line.input.slice(0, -1);
-        return line;
-      });
-    } else if (e.keyCode === 32 || (e.keyCode >= 49 && e.keyCode <= 90) || e.keyCode === 190 || e.keyCode === 191) { // Valid keys for input (space, 0-9, a-Z, period, forward slash)
-      this.updateCurrentLine((line) => {
-        line.input += e.key;
-        return line;
-      });
-    }
-  }
+  // handleOnKeyDown = (e: KeyboardEvent) => {
+  //   if (e.keyCode === 13) { // Enter
+  //     this.handleEnter();
+  //   } else if (e.keyCode === 8) { // Backspace
+  //     this.updateCurrentLine((line) => {
+  //       line.input = line.input.slice(0, -1);
+  //       return line;
+  //     });
+  //   } else if (e.keyCode === 32 || (e.keyCode >= 49 && e.keyCode <= 90) || e.keyCode === 190 || e.keyCode === 191) { // Valid keys for input (space, 0-9, a-Z, period, forward slash)
+  //     this.updateCurrentLine((line) => {
+  //       line.input += e.key;
+  //       return line;
+  //     });
+  //   }
+  // }
 
   renderLines() {
     let lines = [];
+    let autofocus = false;
     for (let i = 0; i < this.state.lines.length; i++) {
+      autofocus = i === this.state.lines.length - 1 ? true : false;
       lines.push(
         <div key={i} className="terminal-line">
           <span className="terminal-prompt">{this.state.lines[i].directory} {this.props.prompt}</span>
-          <TerminalInput text={this.state.lines[i].input} />
+          <TerminalInput autofocus={autofocus} text={this.state.lines[i].input} handleSubmitFunction={(input: string) => this.handleSubmitInput(input)} />
           <div className="terminal-output">{this.state.lines[i].output}</div>
         </div>
       );
--- src/ts/components/TerminalInput.tsx
+++ src/ts/components/TerminalInput.tsx
@@ -2,16 +2,28 @@ import * as React from "react";
 
 import "../../css/terminal-input.css";
 
-interface TerminalInputProps { text: string; }
+interface TerminalInputProps { text: string; handleSubmitFunction: (input: string) => void; autofocus: boolean; }
+interface TerminalInputState { readonly: boolean; }
 
-export class TerminalInput extends React.Component<TerminalInputProps, {}> {
+export class TerminalInput extends React.Component<TerminalInputProps, TerminalInputState> {
   constructor(props: TerminalInputProps) {
     super(props);
+    this.state = { readonly: false };
+  }
+
+  handleOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.keyCode === 13) { // Enter
+      e.preventDefault();
+      this.setState({ readonly: true });
+      this.props.handleSubmitFunction(e.currentTarget.value);
+    } else {
+      console.debug(`Terminal input recieved key "${e.key}"`);
+    }
   }
 
   render() {
     return (
-      <input readOnly={true} className="terminal-input" value = {this.props.text} />
+      <input autoFocus={this.props.autofocus} readOnly={this.state.readonly} className="terminal-input" onKeyDown={this.handleOnKeyDown} />
     )
   }
 }
