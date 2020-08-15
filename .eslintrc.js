98683359caa4b7875a89e2626a732fd68ea48cc3
Joe Sweeney <joe@j1.io>
2020-08-15 13:38:42 -0400

    Add type-aware checking to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsConfigRootDir: __dirname,
+    project: ['./tsconfig.dev.json'],
+  },
   plugins: [
     '@typescript-eslint',
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
 };
