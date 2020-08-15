fbdabd5e00fd5f62ed413938df2df6beab9d7738
Joe Sweeney <joe@j1.io>
2020-08-15 17:10:34 -0400

    Add Node ESLint support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
+const baseExtends = [
+  'eslint:recommended',
+  'plugin:@typescript-eslint/recommended',
+  'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  'plugin:react/recommended',
+  'plugin:import/errors',
+  'plugin:import/warnings',
+  'plugin:import/typescript',
+]
+
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
@@ -8,18 +18,16 @@ module.exports = {
   plugins: [
     '@typescript-eslint',
   ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-  ],
+  extends: baseExtends,
   settings: {
     react: {
       version: 'detect',
     },
   },
+  overrides: [
+    {
+      files: ['*.js', 'src/ts/BuildLocalManifest.ts'],
+      extends: baseExtends.concat(['plugin:node/recommended']),
+    }
+  ]
 };
